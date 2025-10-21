43f5a2ff70f2afe098312f8e9d0d11a0a736aa92
Papamundodev <j.fernandez-ext@matawan-mobility.com>
2025-10-21 12:30:59 -0500

    nova repo

--- functions.php
+++ functions.php
@@ -1,10 +1,14 @@
 <?php
 
 use Theme_base\Base;
+use Theme_base\CustomPostType;
 
 if (is_file(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
+
+// Expose menus publicly (be selective in production!)
+add_filter( 'rest_menu_read_access', '__return_true' );
 
 
 
@@ -14,3 +18,7 @@ $base->themeSupports();
 $base->registerMenus();
 $base->includeStyles();
 $base->includeScripts();
+
+
+$services = new CustomPostType('theme_base', 'service', 'Service', 'Services', 2, 'Description');
+add_action('init', [$services, 'register']);
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'joandev/theme_base',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3bfbdfa7f5cf53beabdf54a8a1d76a73626977fb',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'joandev/theme_base' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3bfbdfa7f5cf53beabdf54a8a1d76a73626977fb',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
